811539626b24808ebf14cbc30cb439a755adf1c5
Antoine Roy-Gobeil <antoine@plot.ly>
2019-08-01 17:40:15 -0400

    deploy 3b9d3bd83e4c96304924b1afdefae0e65b50c091

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s inherit_errexit
+
+git checkout master
+git pull
+CURRENT=$(git rev-parse HEAD)
+npm i
+npm run build
+git checkout gh-pages
+cp -R public/* .
+rm *.map
+git add .
+git commit -m "deploy commit $CURRENT"
+git push
